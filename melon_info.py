6d833678257d4b63d13fa4f39a4a6a37f9758a58
vagrant <vagrant@vagrant.vm>
2020-10-05 17:41:13 +0000

    Track flesh, find, weight

--- melon_info.py
+++ melon_info.py
@@ -1,10 +1,11 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import (melon_names, melon_seedlessness, melon_prices, 
+melon_flesh_color, melon_weight, melon_rind_color)
 
 
-def print_melon(name, seedless, price):
+def print_melon(name, seedless, price, fleshcolor, weight, rindcolor):
     """Print each melon with corresponding attribute information."""
 
     have_or_have_not = 'have'
@@ -12,7 +13,14 @@ def print_melon(name, seedless, price):
         have_or_have_not = 'do not have'
 
     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    print(f"{name}: \n"
+            f"seedless: {seedless}\n" 
+            f"price: {price}\n"
+            f"flesh color: {fleshcolor}\n"
+            f"weight: {weight}\n" 
+            f"rind color: {rindcolor}\n")
 
 
 for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i],
+                melon_flesh_color[i], melon_weight[i], melon_rind_color[i])
