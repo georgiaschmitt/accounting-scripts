1e5d4b567d77783632b1b49e4dad1f29a3dbdcc6
vagrant <vagrant@vagrant.vm>
2020-10-05 18:10:24 +0000

    Refactored dictionary to facilitate new attributes

--- melon_info.py
+++ melon_info.py
@@ -1,26 +1,19 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import (melon_names, melon_seedlessness, melon_prices, 
-melon_flesh_color, melon_weight, melon_rind_color)
+from melons import melon_data
 
 
-def print_melon(name, seedless, price, fleshcolor, weight, rindcolor):
+def print_melon(melon_data):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for melon_name, attributes in melon_data.items():
+        print(melon_name.upper())
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-    print(f"{name}: \n"
-            f"seedless: {seedless}\n" 
-            f"price: {price}\n"
-            f"flesh color: {fleshcolor}\n"
-            f"weight: {weight}\n" 
-            f"rind color: {rindcolor}\n")
+        for attribute, value in attributes.items():
+            print(f'{attribute}: {value}')
+
+        print('===================================')
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i],
-                melon_flesh_color[i], melon_weight[i], melon_rind_color[i])
+print_melon(melon_data)
--- melons.py
+++ melons.py
@@ -1,47 +1,6 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
-
-melon_flesh_color = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-}
-
-melon_weight = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-}
-
-melon_rind_color = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-}
+melon_data = {
+    'Honeydew': {'price': 0.99, "seedlessness": True, "fleshcolor": None, "weight": None, "rindcolor": None},
+    'Crenshaw' : {'price': 2.00, "seedlessness": False, "fleshcolor": None, "weight": None, "rindcolor": None},
+    'Crane' : {'price': 2.50, "seedlessness": False, "fleshcolor": None, "weight": None, "rindcolor": None},
+    'Casaba' : {'price': 2.50, "seedlessness": False, "fleshcolor": None, "weight": None, "rindcolor": None},
+    'Cantaloupe' : {'price': 0.99, "seedlessness": False, "fleshcolor": None, "weight": None, "rindcolor": None}}
